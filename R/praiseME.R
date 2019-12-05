a7182d658fcf7dee28b1b08fc4cbc83c16a7e44a
Alishum Ali <alishumali1@gmail.com>
2019-12-05 16:16:30 +1100

    deleted due to using roxigen2

---
+++ R/praiseME.R
@@ -0,0 +1,18 @@
+#' A function that praises the user
+#'
+#'
+#'\code{praiseME} returns a praise incorporating a user-speciefied string.
+#' @param ali
+#'
+#' @return A string with the praise.
+#' @export
+#'
+#' @examples
+#'
+#' praiseME("Ali")
+praiseME <- function(x){
+
+    paste("Your are amazing", x)
+}
+
+praiseME("ali")
